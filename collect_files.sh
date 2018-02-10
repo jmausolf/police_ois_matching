397f7cee52940b1bf8c85a92bbe4e090cdd1fa0d
jmausolf <jgm346@gmail.com>
2018-02-10 00:56:35 -0600

    minor adjustment to file organization

--- collect_files.sh
+++ collect_files.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-mkdir -p downloads 2>/dev/null
+mkdir -p downloads
 mv *.csv downloads 2>/dev/null
 mv *.tsv downloads 2>/dev/null
 mv *.zip downloads 2>/dev/null
 mv *.wget downloads 2>/dev/null
+
+mkdir -p data
