c885c6aab76f89297077d480e67226c562756295
jmausolf <jgm346@gmail.com>
2018-02-28 00:22:45 -0600

    updates to analysis

--- analysis.R
+++ analysis.R
@@ -19,20 +19,6 @@ for (cs in cs_types) {
 
 #General Pre-QC Processing for each DF
 
-# df <- wp_df %>% 
-#   mutate(police = if_else(police == TRUE, TRUE, FALSE, FALSE)) %>% 
-#   mutate(crowd = if_else(crowd == TRUE, TRUE, FALSE, FALSE)) %>% 
-#   mutate(match = if_else(police == TRUE & crowd == TRUE, "yes_match", "no_match")) %>%
-#   mutate(match = if_else(match == "no_match" & police == TRUE & crowd == FALSE, "no_match_crowd_missing", match)) %>%
-#   mutate(match = if_else(match == "no_match" & crowd == TRUE & police == FALSE, "no_match_police_missing", match)) %>%
-#   mutate(outcome = if_else(police == TRUE, outcome, "unknown_police_missing", "unknown_police_missing")) %>% 
-#   select(date, name, police, crowd, match, outcome, everything()) %>% 
-#   unique() %>% 
-#   arrange(match) %>% 
-#   filter(outcome == "deceased" | outcome == "unknown_police_missing")
-
-
-
 refine_matches <- function(mdf, ois_type) {
   
   df <- mdf %>% 
@@ -58,44 +44,8 @@ refine_matches <- function(mdf, ois_type) {
   return(df)
 }
 
-#wp_df_all <- refine_matches(wp_df, "all")
-#gd_df_all <- refine_matches(gd_df, "all")
-#ds_df_all <- refine_matches(ds_df, "all")
-#ds_df_all <- refine_matches(ds_df, "all")
 
-#WP Cleaning
-wp_df_deceased <- refine_matches(wp_df, "deceased")
-
-
-#GD Cleaning
-gd_df_deceased <- refine_matches(gd_df, "deceased") %>%
-  mutate(date_qc = date, 
-         name_qc = name,
-         match_qc = match) %>% 
-  rename(pd = lawenforcementagency) %>% 
-  mutate(pd = if_else(is.na(pd) & police == TRUE, "Dallas Police Department", pd)) %>% 
-  mutate(uof = if_else(is.na(classification) & police == TRUE, "Gunshot", classification)) %>% 
-  select(date, date_qc, name, name_qc, police, crowd, match, match_qc, outcome, pd, uof, everything()) %>% 
-  #Remove Cases That Are Not Dallas PD
-  filter(pd == "Dallas Police Department") %>%
-  #Remove Cases That Are Not OIS
-  filter(!(uof !=  "Gunshot" & match != "yes_match")) %>% 
-  #Alter qc details for elias portillo case
-  mutate(date_qc = if_else(date == "2016-08-24" & name =="elias portillo", as.Date("2016-08-25"), date_qc),
-         name_qc = if_else(date == "2016-08-25" & name =="unknown", "elias portillo", name_qc),
-         match_qc = if_else(date_qc == "2016-08-25" & name_qc =="elias portillo", "yes_match", match_qc)) %>% 
-  distinct(date_qc, name_qc, match_qc) 
-
-
-
-
-
-
-#DS Cleaning
-ds_df_deceased <- refine_matches(ds_df, "deceased")
-ds_df_nonfatal <- refine_matches(ds_df, "non_fatal")
-
-
+#Custom Custom Table Functions
 
 #Tables
 save_stargazer <- function(output.file, ...) {
@@ -144,36 +94,181 @@ summary_row <- function(df) {
 }
 
 
+
+#WP Cleaning
+wp_df_deceased <- refine_matches(wp_df, "deceased") %>% 
+  mutate(date_qc = date, 
+         name_qc = name,
+         match_qc = match) %>% 
+    mutate(uof = if_else(is.na(mannerofdeath) & police == TRUE, "shot", mannerofdeath)) %>% 
+    select(date, date_qc, name, name_qc, police, crowd, match, match_qc, outcome, uof, everything()) %>% 
+    #Missing police files could be out of jurisdiction, but WP does not have the pd responsible
+    filter(!(uof !=  "shot" & match != "yes_match")) %>% 
+    #Alter qc details for elias portillo case
+    mutate(name_qc = if_else(date == "2016-08-25" & name =="unknown", "elias portillo", name_qc)) %>% 
+    distinct(date_qc, name_qc, match_qc) %>% 
+    mutate(matches = (if_else(match_qc == "yes_match", 1, 0)),
+           no_match_police = if_else(match_qc == "no_match_police_missing", 1, 0),
+           no_match_crowd = if_else(match_qc == "no_match_crowd_missing", 1, 0))
+
+
 #Create Desired DF Table
-#make function to prep table data
-gddf <- gd_df_deceased %>% 
+wpdf <- make_pd_df()[-1,] %>% 
+  add_pd_case("Dallas Police Department", summary_row(wp_df_deceased)) %>% 
+  rename("Matched" = Matches,
+         "Not Matched by Crowd" = No_Match_Crowd,
+         "Not Matched by Police" = No_Match_Police)
+
+#Save Table
+save_stargazer("table1.tex", as.data.frame(wpdf), header=FALSE, type='latex',
+               font.size = "footnotesize",
+               title = "Matched Reports of Fatal Officer Involved Shootings - Washington Post",
+               notes  = "Notes: Match Metrics by Police Department, Matches and Mismatches by Source Displayed as Proportions",
+               summary = FALSE)
+
+
+
+
+#GD Cleaning
+gd_df_deceased <- refine_matches(gd_df, "deceased") %>%
+  mutate(date_qc = date, 
+         name_qc = name,
+         match_qc = match) %>% 
+  rename(pd = lawenforcementagency) %>% 
+  mutate(pd = if_else(is.na(pd) & police == TRUE, "Dallas Police Department", pd)) %>% 
+  mutate(uof = if_else(is.na(classification) & police == TRUE, "Gunshot", classification)) %>% 
+  select(date, date_qc, name, name_qc, police, crowd, match, match_qc, outcome, pd, uof, everything()) %>% 
+  #Remove Cases That Are Not Dallas PD
+  filter(pd == "Dallas Police Department") %>%
+  #Remove Cases That Are Not OIS
+  filter(!(uof !=  "Gunshot" & match != "yes_match")) %>% 
+  #Alter qc details for elias portillo case
+  mutate(date_qc = if_else(date == "2016-08-24" & name =="elias portillo", as.Date("2016-08-25"), date_qc),
+         name_qc = if_else(date == "2016-08-25" & name =="unknown", "elias portillo", name_qc),
+         match_qc = if_else(date_qc == "2016-08-25" & name_qc =="elias portillo", "yes_match", match_qc)) %>% 
+  distinct(date_qc, name_qc, match_qc) %>% 
   mutate(matches = (if_else(match_qc == "yes_match", 1, 0)),
          no_match_police = if_else(match_qc == "no_match_police_missing", 1, 0),
          no_match_crowd = if_else(match_qc == "no_match_crowd_missing", 1, 0))
 
 
-df <- make_pd_df()[-1,] %>% 
-  add_pd_case("Dallas Police Department", summary_row(gddf)) %>% 
-  rename("No Matches Crowd" = No_Match_Crowd,
-         "No Matches Police" = No_Match_Police)
+#Create Desired DF Table
+gddf <- make_pd_df()[-1,] %>% 
+  add_pd_case("Dallas Police Department", summary_row(gd_df_deceased)) %>% 
+  rename("Matched" = Matches,
+         "Not Matched by Crowd" = No_Match_Crowd,
+         "Not Matched by Police" = No_Match_Police)
+
+
+#Save Table
+save_stargazer("table2.tex", as.data.frame(gddf), header=FALSE, type='latex',
+               font.size = "footnotesize",
+               title = "Matched Reports of Fatal Officer Involved Shootings - The Guardian",
+               notes  = "Notes: Match Metrics by Police Department, Matches and Mismatches by Source Displayed as Proportions",
+               summary = FALSE)
+
+
+
+
+#DS Cleaning
+ds_df_all <- refine_matches(ds_df, "all") %>% 
+  mutate(name = if_else(is.na(name), "error_unknown", name)) %>% 
+  mutate(date_qc = date, 
+         name_qc = name,
+         match_qc = match,
+         outcome_qc = outcome) %>%
+  rename(pd = agencyname) %>% 
+  mutate(pd = if_else(is.na(pd) & police == TRUE, "Dallas Police Department", pd),
+         pd = if_else(pd == "Dallas Police", "Dallas Police Department", pd),
+         pd = if_else(pd == "Dallas PD", "Dallas Police Department", pd)) %>% 
+  select(date, date_qc, name, name_qc, match, match_qc, outcome, outcome_qc, everything()) %>% 
+  #Remove Cases That Are Not Dallas PD
+  filter(pd == "Dallas Police Department") %>%
+  #Alter qc details for james harper case
+  mutate(date_qc = if_else(date == "2012-08-24" & name =="james harper", as.Date("2012-07-24"), date_qc),
+         date_qc = if_else(date == "2012-12-24" & name =="james harper", as.Date("2012-07-24"), date_qc),
+         name_qc = if_else(date == "2012-07-24" & name =="james jarper", "james harper", name_qc),
+         match_qc = if_else(date_qc == "2012-07-24" & name_qc =="james harper", "yes_match", match_qc),
+         outcome_qc = if_else(name_qc == "james harper", "deceased", outcome_qc)) %>% 
+  #Alter qc details for name parse errors dpd
+  mutate(name_qc = if_else(date == "2012-08-03" & is.na(name), "roberto gabaldon", name_qc),
+         name_qc = if_else(date == "2012-12-14" & is.na(name), "lenny ellis", name_qc)) %>% 
+  #Alter qc details for roberto gabaldon case
+  mutate(match_qc = if_else(date_qc == "2012-08-03" & name_qc =="roberto gabaldon", "yes_match", match_qc),
+         outcome_qc = if_else(name_qc == "roberto gabaldon", "injured", outcome_qc)) %>% 
+  #Alter qc details for stoney eugene rawlinson case
+  mutate(date_qc = if_else(date == "2013-02-09" & name =="stoney eugene rawlinson", as.Date("2013-02-08"), date_qc),
+         name_qc = if_else(date == "2013-02-08" & name =="rawlison stoney", "stoney rawlinson", name_qc),
+         name_qc = if_else(date == "2013-02-09" & name =="stoney eugene rawlinson", "stoney rawlinson", name_qc),
+         match_qc = if_else(date_qc == "2013-02-08" & name_qc =="stoney rawlinson", "yes_match", match_qc),
+         outcome_qc = if_else(name_qc == "stoney rawlinson", "deceased", outcome_qc)) %>% 
+  distinct(date_qc, name_qc, match_qc, outcome_qc) %>% 
+  arrange(match_qc) %>% 
+  mutate(matches = (if_else(match_qc == "yes_match", 1, 0)),
+         no_match_police = if_else(match_qc == "no_match_police_missing", 1, 0),
+         no_match_crowd = if_else(match_qc == "no_match_crowd_missing", 1, 0))
+
+
+ds_df_deceased <- ds_df_all %>% 
+  filter(outcome_qc == "deceased")
+
+ds_df_nonfatal <- ds_df_all %>% 
+  filter(outcome_qc != "deceased")
+
+#Create Desired DF Table
+dsdf <- make_pd_df()[-1,] %>% 
+  add_pd_case("Dallas Police Department - Fatal", summary_row(ds_df_deceased)) %>% 
+  add_pd_case("Dallas Police Department - Non Fatal", summary_row(ds_df_nonfatal)) %>% 
+  rename("Matched" = Matches,
+         "Not Matched by Crowd" = No_Match_Crowd,
+         "Not Matched by Police" = No_Match_Police)
+
+
+#Save Table
+save_stargazer("table3.tex", as.data.frame(dsdf), header=FALSE, type='latex',
+               font.size = "footnotesize",
+               title = "Matched Reports of Fatal and Non-Fatal Officer Involved Shootings - Deadspin",
+               notes  = "Notes: Match Metrics by Police Department, Matches and Mismatches by Source Displayed as Proportions",
+               summary = FALSE)
+
+
+
+#Graph Data
+wp <- wp_df_deceased %>%
+  mutate(outcome = "Fatal") %>% 
+  mutate(source = "Washington Post")
+
+gd <- gd_df_deceased %>%
+  mutate(outcome = "Fatal") %>% 
+  mutate(source = "The Guardian")
   
+ds <- ds_df_all %>% 
+  mutate(outcome = if_else(outcome_qc == "deceased", "Fatal", "Non-Fatal")) %>% 
+  mutate(source = "Deadspin") %>% 
+  select(-outcome_qc) 
+
+df <- rbind(wp, gd, ds) %>% 
+  mutate(outcome = factor(outcome),
+         source = factor(source)) %>% 
+  mutate(match_qc = factor(match_qc, 
+      levels = c("no_match_police_missing", "no_match_crowd_missing", "yes_match"),
+      labels = c( "Not Matched by Police", "Not Matched by Crowdsource", "Matched"))) 
 
 
-#for testing
-# stargazer(as.data.frame(df), header=FALSE, type='html',
-#           font.size = "scriptsize",
-#           title = "Data Matches and Completeness: Guardian - Counted by Police Department",
-#           dep.var.caption  = "Match Metrics by Police Department",
-#           dep.var.labels   = "Matches and Mismatches by Source as Proportions",
-#           summary = FALSE)
-
-#saving
-save_stargazer("table1.tex", as.data.frame(df), header=FALSE, type='latex',
-          font.size = "scriptsize",
-          title = "Matched Reports of Fatal Officer Involved Shootings - The Guardian",
-          notes  = "Notes: Match Metrics by Police Department, Matches and Mismatches by Source Displayed as Proportions",
-          summary = FALSE)
+##Make directory
+system('mkdir -p images')
 
 
-
+#Graph
+ggplot(df) +
+  geom_bar(aes(source, fill = match_qc), alpha=1, position = "fill") +
+  facet_grid(.~outcome) +
+  scale_fill_manual(values=c("#2174B0", "#093E63", "#BF1200")) +
+  xlab("Crowd Source") +
+  ylab("Proportion of Matches and Non Matches by Type") +
+  ggtitle(paste("Matched Reports of Officer Involved Shootings")) +
+  theme(legend.position="bottom") +
+  theme(legend.title=element_blank()) + 
+  theme(plot.title = element_text(hjust = 0.5))
+ggsave("images/plt1.png", width = 8, height = 4.8)
 
