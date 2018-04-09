3d5beae8181e09b453c7ee0159850cb428a43940
jmausolf <jgm346@gmail.com>
2018-04-09 16:41:01 -0500

    removing middle names initials, suffixes from all data sources

--- clean.py
+++ clean.py
@@ -31,11 +31,55 @@ def remove_punct(text):
 
 def rm_mid_initials_suffixes(var, df):
 
+    #replace text preceding ".", e.g. jr. sen.
     s = df[var].str.replace(r"[a-z]+\.", '')
-    s = s.str.replace(' jr', ' ').str.replace(' sr', ' ').str.strip()
+
+    #replace text following a comma
+    #ensure names are reversed previously
+    s = s.str.replace(r"\,\s[a-z]+", '')
+
+
+    s = s.str.replace(' jr', ' ').str.replace(' junior', ' ')
+    s = s.str.replace(' sr', ' ').str.replace(' senior', ' ')
+    s = s.str.replace(' iii', ' ').str.replace(' third', ' ').str.replace(' the third', ' ')
+    s = s.str.replace(' iv', ' ').str.replace(' fourth', ' ').str.replace(' the fourth', ' ')
+    s = s.str.replace(' v', ' ').str.replace(' fifth', ' ').str.replace(' the fifth', ' ')
+    s = s.str.strip()
+
+    #replace more than one white space with a space
     s = s.str.replace(r"\s{2,}", ' ')
+
     s.name = var
-    df = df.drop(var, axis=1).join(s)
+    df = df.drop(var, axis=1)
+    df = pd.concat([df, s], axis=1)
+    return(df)
+
+
+
+def keep_first_last(namelist):
+    try:
+        if len(namelist) >= 3:
+            first = namelist[0]
+            last = namelist[-1]
+            first_last = [first, last]
+            name = " ".join(first_last).strip()
+
+        else:
+            name = " ".join(namelist).strip()
+
+        return name
+
+    except Exception as e:
+        print("[*] Error: {}, exception when removing middle name...".format(e))
+        return namelist
+
+def rm_middle_name(var, df):
+    s = df[var].fillna('unknown')
+    s = s.str.split(r'\s')
+    s = s.apply(lambda name: keep_first_last(name))
+    s.name = var
+    df = df.drop(var, axis=1)
+    df = pd.concat([df, s], axis=1)
     return(df)
 
 
@@ -149,10 +193,12 @@ def clean_dfw_police_ois():
     df = split_subjects('subjects', df)
     df = split_race_gender(df)
     df = reverse_names('name', df)
+    df = rm_mid_initials_suffixes('name', df)
+    df = rm_middle_name('name', df)
     ren('subjectdeceasedinjuredorshootandmiss', 'outcome', df)
     ren('attorneygeneralformsurl', 'ag_url', df)
     df = lower_var('outcome', df)
-    print(df.head(10))
+    print(df.head(5))
     df.to_csv('{}_cleaned.csv'.format(infile), index=False)
 
 
@@ -164,9 +210,11 @@ def clean_den_police_ois():
     df = split_vars('citystate', 'city', 'state', ',', df)
     df = reverse_names('lastfirstname', df, delim=',')
     df = ren('lastfirstname', 'name', df)
+    df = rm_mid_initials_suffixes('name', df)
+    df = rm_middle_name('name', df)
     df = ren('incidentdate', 'date', df)
     df = lower_var('casualty', df)
-    print(df.head(10))
+    print(df.head(5))
     df.to_csv('{}_cleaned.csv'.format(infile), index=False)
 
 
@@ -178,6 +226,7 @@ def clean_jax_police_ois():
     df = reverse_names('suspectname', df, delim=',')
     df = ren('suspectname', 'name', df)
     df = rm_mid_initials_suffixes('name', df)
+    df = rm_middle_name('name', df)
     df = ren('incidentnbr', 'incidentnumber', df)
     df = split_vars('incidentdate', 'date', 'time', ' ', df)
     df = lower_var('suspectshot', df)
@@ -186,7 +235,7 @@ def clean_jax_police_ois():
     df.loc[(df.fatal=='yes'), 'outcome'] = 'deceased'
     df.loc[((df.fatal=='no') & (df.suspectshot=='yes')), 'outcome'] = 'shot_alive'
     df.loc[((df.fatal=='no') & (df.suspectshot=='no')), 'outcome'] = 'not_shot_alive'  
-    print(df.head(10))
+    print(df.head(5))
     df.to_csv('{}_cleaned.csv'.format(infile), index=False)
 
 #clean_jax_police_ois()
@@ -198,6 +247,9 @@ def clean_wp_crowdsource():
     df = pd.read_csv('{}.csv'.format(infile))
     df = clean_cols(df)
     df = lower_var('name', df)
+    df = rm_mid_initials_suffixes('name', df)
+    df = rm_middle_name('name', df)
+    print(df.head(5))
     df.to_csv('{}_cleaned.csv'.format(infile), index=False)
 
 
@@ -207,12 +259,12 @@ def clean_gv_crowdsource():
     print(infile)
     df = pd.read_csv('{}.tsv'.format(infile), delimiter='\t', encoding='utf-8')
     df = clean_cols(df)
-
     df = split_subjects_gv('infoaboutparticipants', df)
     df = map_dict_col('infoaboutparticipants', df)
     df = lower_var('name', df)
-
-    #TODO remove unicode punct
+    df = rm_mid_initials_suffixes('name', df)
+    df = rm_middle_name('name', df)
+    print(df.head(5))
     df.to_csv('{}_cleaned.csv'.format(infile), index=False)
 
 
@@ -221,12 +273,14 @@ def clean_gd_crowdsource():
     infiles = glob('downloads/{}*{}*.csv'.format('gd', 'crowdsource'))
     infiles = [file.replace('.csv', '') for file in infiles]
     outfile = infiles[0].rsplit('-', 1)[0]
-
     df1 = pd.read_csv('{}.csv'.format(infiles[0]))
     df2 = pd.read_csv('{}.csv'.format(infiles[1]))
     df = df1.append(df2, ignore_index=True)
     df = ren('raceethnicity', 'race', df)
     df = lower_var('name', df)
+    df = rm_mid_initials_suffixes('name', df)
+    df = rm_middle_name('name', df)
+    print(df.head(5))
     df.to_csv('{}_cleaned.csv'.format(outfile), index=False)
 
 
@@ -239,8 +293,11 @@ def clean_ds_crowdsource():
     df = ren('name', 'other_name', df)
     df = ren('victimname', 'name', df)
     df = lower_var('name', df)
+    df = rm_mid_initials_suffixes('name', df)
+    df = rm_middle_name('name', df)
     df = title_var('city', df)
     df = split_vars('state', 'state_abv', 'state_name', '-', df)
+    print(df.head(5))
     df.to_csv('{}_cleaned.csv'.format(infile), index=False)
 
 
